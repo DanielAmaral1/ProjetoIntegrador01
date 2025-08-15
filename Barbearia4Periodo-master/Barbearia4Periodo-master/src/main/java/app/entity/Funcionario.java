a6dc78722b013478efd6ebc409834ad1462adb59
Daniel Amaral <danielspiesamaral@gmail.com>
2025-08-14 21:12:01 -0300

    colocando dnv a classe produto e as validaçoes com not

--- Barbearia4Periodo-master/Barbearia4Periodo-master/src/main/java/app/entity/Funcionario.java
+++ Barbearia4Periodo-master/Barbearia4Periodo-master/src/main/java/app/entity/Funcionario.java
@@ -1,57 +1,33 @@
 package app.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 public class Funcionario {
 
     @Id
-    private int id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    @NotBlank(message = "O campo nome é obrigatório")
     private String nome;
+
+    @NotBlank(message = "O campo telefone é obrigatório")
     private String telefone;
+
+    @NotBlank(message = "O campo endereço é obrigatório")
     private String endereco;
 
-    public Funcionario() {
-    }
+    public Funcionario() {}
 
-    public Funcionario(int id, String nome, String telefone, String endereco) {
-        this.id = id;
+    public Funcionario(String nome, String telefone, String endereco) {
         this.nome = nome;
         this.telefone = telefone;
-        this.endereco = endereco;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
-
-    public String getEndereco() {
-        return endereco;
-    }
-
-    public void setEndereco(String endereco) {
         this.endereco = endereco;
     }
 }
